8adf797f1795e5dd3e818a9d984a5db45aa59263
Qudus <muyiwasalau@gmail.com>
2021-06-17 13:50:44 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 /**
- *printf - prints "with proper grammar, but the outcome is a piece of art,"
- * main - void
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
