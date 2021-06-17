f2a35696bf9a561acca45821fad5ab134319c563
Qudus <muyiwasalau@gmail.com>
2021-06-17 13:55:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-
-#include<stdio.h>
-int main()
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
 {
-  char Mystr[] = "Programming is like building a multilingual puzzle\n";
-  puts(Mystr);
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
