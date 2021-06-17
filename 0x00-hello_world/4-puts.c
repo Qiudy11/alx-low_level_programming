3c4cdbfbee22ad6af7c3354b705484a47ca7c3ad
Qudus <muyiwasalau@gmail.com>
2021-06-17 13:32:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-  char Mystr[] = "Programming is like building a multilingual puzzle";
+  char Mystr[] = "Programming is like building a multilingual puzzle\n";
   puts(Mystr);
     return 0;
 }
