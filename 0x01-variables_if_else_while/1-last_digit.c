9572703a8a7a8ed5e2f3f1e0cc883c63d72b334f
Qudus <muyiwasalau@gmail.com>
2021-06-18 10:56:26 -0700

    alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -15,9 +16,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("Last digit of %d is %d %s\n", n, n % 10,
-		((n % 10) == 0) ? "and is 0"
-			: ((n % 10) > 5) ? "and is greater than 5"
-				: "and is less than 6 and not 0"));
+	if (n > 5)
+	  {
+	    printf("and is greater than 5\0", n);  
+	  }
+	else if (n < 6; != 0)
+	  {
+	    printf("and is less than 6 and not 0\n", n);
+	  }
+	else (n = 0)
+	       {
+		 printf("is zero\n", n);
+	       }
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints the alphabets in lowercase, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main()
+{
+for(int x = 'a'; x <= 'z'; x++)
+    {
+putchar(x);
+    }
+return 0;
+}
