a11bd8545763741e64ed1a23c811c69996584aad
Qudus <muyiwasalau@gmail.com>
2021-06-19 06:15:46 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
 *main - main entry
 *Desription: program that prints all single digit numbers of base 10 starting from 0
@@ -8,7 +7,7 @@
 int main(void)
 {
 int n;
-for(n = '0'; n <='9'; n++)
+for (n = '0'; n <= '9'; n++)
 {
 putchar(n);
 }
