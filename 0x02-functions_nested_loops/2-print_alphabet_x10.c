42301fd2fa39b066cbf2e2f215342b2f164c776f
Qudus <muyiwasalau@gmail.com>
2021-06-22 07:08:54 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+void print_alphabet_x10(void)
+{
+int i = 0, j;
+  while (i<10)
+    {
+    j = 0;
+    while(j<1)
+    {
+ for(j = 0; j < 26; ++j)
+    _putchar('a' + j);
+    {
+    _putchar('\n');
+    }
+    }
+        _putchar('\n');
+        i++;
+  }
+}
