3e5e71529a869ac620c2e03115a7561978d9a75e
Qudus <muyiwasalau@gmail.com>
2021-06-25 03:46:10 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_numbers - Print the numbers since 0 up to 9
+*
+* Return: The numbers since 0 up to 9
+*/
+void print_numbers(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
