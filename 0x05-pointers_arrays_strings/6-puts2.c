0af5b4c6d2978dfa0021fe6498d37f963b3bc591
Qudus <muyiwasalau@gmail.com>
2021-06-28 12:56:45 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*puts2 - print every other character followed by a new line
+*@str : pointer of type char.
+*
+*
+*Return : always 0.
+*/
+void puts2(char *str)
+{
+int i;
+for (i =0; *str != '\0'; i++)
+{
+if (i % 2 == 0)
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
