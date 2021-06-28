7ac7d6c703aa56eb0d0d6058ab1b503c416c70b2
Qudus <muyiwasalau@gmail.com>
2021-06-28 12:08:38 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*_strlen - returns the length of a string
+*@s: is a pointer char type
+*Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; *s != '\0'; len++)
+s++;
+return (len);
+}
