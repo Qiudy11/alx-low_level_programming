1a964fab3da2dded0aa1bb3ff418cc69c576b0d6
Qudus <muyiwasalau@gmail.com>
2021-06-28 12:23:34 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*_puts - Prints a string, followed by a new line to stdout
+*@str : Pointer of type char
+*
+*
+*
+*Return : always 0.
+*/
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
