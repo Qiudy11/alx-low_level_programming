18303a5faec93d3faefb5a7c813b20998fe70835
Qudus <muyiwasalau@gmail.com>
2021-06-28 11:48:13 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-*swap - check the code for Holberton School Students.
-*@int *a: is a pointer int type
-*@int *b: is a pointer int type
+*swap_int - check the code for Holberton School Students.
+*@*a: is a pointer int type
+*@*b: is a pointer int type
 * Return: Always 0.
 */
 void swap_int(int *a, int *b)
