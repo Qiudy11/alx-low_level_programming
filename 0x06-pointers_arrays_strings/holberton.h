f02bb7ec8d79a3434fd13bd288d817e7b77e91cc
qiudy11 <muyiwasalau@gmail.com>
2021-07-01 05:06:08 -0700

    header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,6 +2,7 @@
 #define HOLBERTON_FUNTIONS
 
 int _putchar(char c);
+int *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
