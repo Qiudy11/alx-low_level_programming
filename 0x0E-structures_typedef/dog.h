51e798c6c0f1a866e62f507c8eee73db928fa70b
muyiwa <muyiwasalau@gmail.com>
2021-07-19 13:48:39 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,15 @@
 #ifndef _dog_h
 #define _dog_h_
 /**
- * struct dog - such structure, wow
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- */
-
+* struct dog - such structure, wow
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-
 #endif /* _dog_h_ */
