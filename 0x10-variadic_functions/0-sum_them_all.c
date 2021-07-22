bf90d8be7252f3b901544a272fc5b698baee6675
muyiwa <muyiwasalau@gmail.com>
2021-07-22 06:25:50 -0700

    sum of integers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,16 +2,15 @@
 #include "variadic_functions.h"
 /**
 *sum_them_all - sum of integers
-*n : number of arguement
-*
-*return : sum of integers if n != 0
+*@n : number of arguement
+*Return: sum of integers if n != 0
 */
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int sum = 0, i = 0;
 va_list ap;
 va_start(ap, n);
-if( n == 0)
+if (n == 0)
 {
 return (0);
 }
@@ -19,5 +18,5 @@ else
 for (i = 0; i < n; i++)
 sum += va_arg(ap, int);
 va_end(ap);
-return sum;
+return (sum);
 }
