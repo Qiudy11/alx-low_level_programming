53082a4f5fe0324a3786e8eeafcbbdc76d8abae5
Muyiwa <muyiwasalau@gmail.com>
2021-07-29 13:50:55 -0700

    number of element

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+* list_len - fills memory with a constant byte
+* @h: value
+* Return: a number
+*/
+size_t list_len(const list_t *h)
+{
+size_t i;
+for (i = 0; h != NULL; i++)
+h = h->next;
+return (i);
+}
