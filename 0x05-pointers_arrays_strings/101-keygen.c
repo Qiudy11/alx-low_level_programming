41b651ba855c59cf810ecf55f450b521c5aa7242
Muyiwa <muyiwasalau@gmail.com>
2021-08-06 02:26:29 -0700

    Random number generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,18 +9,9 @@
 */
 int main(void)
 {
-char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char password[22];
-int i, n, passw, complem;
-srand(time(0));
-for (i = 0; passw < 2772; i++)
-{
-n = rand() % 10;
-password[i] = seed[n];
-passw += password[i];
-}
-complem = 2772 - passw;
-password[i] = complem;
-printf("%s\n",  password);
-return (0);
+    srand(time(0));
+    for(int i = 0; i<4; i++)
+        printf(" %d ", rand());
+   
+    return 0;
 }
