a8e36745f0abed3096131907215a3415961750d8
Muyiwa <muyiwasalau@gmail.com>
2021-09-16 14:48:58 -0700

    betty coding style and documentation fixed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,19 @@
+/**
+ *dlistint_len - function that prints all elements
+ *@h: head
+ *
+ *Return: number of element
+ */
+
+
 #include "lists.h"
 size_t dlistint_len(const dlistint_t *h)
 {
-  int size = 0;
-  while (h)
-    {
-      size++;
-      h = h->next;
-    }
-  return size;
+int size = 0;
+while (h)
+{
+size++;
+h = h->next;
+}
+return (size);
 }
