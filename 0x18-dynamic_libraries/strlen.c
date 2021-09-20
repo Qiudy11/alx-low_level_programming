34358132ebe89ee04d2c7ca0676d5a1badbad5ab
Muyiwa <muyiwasalau@gmail.com>
2021-09-20 07:31:24 -0700

    all .c files

---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_strlen - returns the length of a string
+*@s: is a pointer char type
+*Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; *s != '\0'; len++)
+s++;
+return (len);
+}
+© 2021 GitHub, Inc.
